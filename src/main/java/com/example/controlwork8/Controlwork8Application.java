c255719d128b9c2aeed469e4e7fc19180d60b042
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-04-29 13:11:37 +0600

    git init, added create cocktails functions

---
+++ src/main/java/com/example/controlwork8/Controlwork8Application.java
@@ -0,0 +1,13 @@
+package com.example.controlwork8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controlwork8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Controlwork8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/controlwork8/Controlwork8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controlwork8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Controlwork8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
